db0ed104dc3c1d0d13c31ac1ecea62f6904f9f78
Jason E Perkins <webdev@abovethefold.biz>
2026-04-23 11:52:39 -0400

    feat: scaffold project architecture with Astro components, Tailwind styling, and asset organization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,47 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig, fontProviders} from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+import alpinejs from '@astrojs/alpinejs'
+import cloudflare from '@astrojs/cloudflare'
+import icon from 'astro-icon'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  adapter: cloudflare({
+    prerenderEnvironment: 'node',
+  }),
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Gelasio',
+      cssVariable: '--font-display',
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Poppins',
+      cssVariable: '--font-body',
+    },
+  ],
+  integrations: [
+    alpinejs({entrypoint: './src/utilities/alpine/index.ts'}),
+    icon({
+      include: {
+        mdi: [
+          'chevron-down',
+          'menu',
+          'moon-waning-crescent',
+          'star',
+          'white-balance-sunny',
+          'window-close',
+        ],
+      },
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
+
+// provider: fontProviders.adobe({ id: 'your-id' })
+// provider: fontProviders.bunny()
+// provider: fontProviders.fontshare()
+// provider: fontProviders.google()
